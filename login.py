19a931e14ab7477bed9abab132825a2ba2cfa22c
Eddie Ivan <33752542+EddieIvan01@users.noreply.github.com>
2018-08-27 10:26:43 +0800

    Update login.py

--- login.py
+++ login.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import time
-from bs64 import Base64 
+from hex2b64 import HB64 
 import RSAJS
 import json
 import getpass
@@ -34,12 +34,13 @@ class httpmthd():
         self.token = soup.find('input',attrs={'id':'csrftoken'}).attrs['value']
 
     def process_public(self,str):               #处理密码,rsa加密
-        self.exponent = Base64().b64_to_hex(self.pub['exponent'])           #将json中的base64加密公钥解密
-        self.modulus = Base64().b64_to_hex(self.pub['modulus'])
+        a = HB64()
+        self.exponent = a.b642hex(self.pub['exponent'])           #将json中的base64加密公钥解密
+        self.modulus = a.b642hex(self.pub['modulus'])
         rsa = RSAJS.RSAKey()
         rsa.setPublic(self.modulus, self.exponent)                          #rsa加密
         cry_data = rsa.encrypt(str)
-        return Base64().hex_to_b64(cry_data)                                #加密后的数据进行base64加密
+        return a.hex2b64(cry_data)                                  #加密后的数据进行base64加密
 
     def post_data(self):                        #post数据
         try:
